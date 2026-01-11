25076a05e70a3e21090cee39d9e7c0499de27527
Povilas Sauciuvienas <p.sauciuvienas@gmail.com>
2026-01-11 18:20:35 +0000

    network arch test

--- PBS_files/test_netowrk_architecture.bash
+++ PBS_files/test_netowrk_architecture.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -N train_walking_0
 #PBS -l select=1:ncpus=16:mem=120gb
-#PBS -l walltime=02:40:00
+#PBS -l walltime=03:40:00
 #PBS -j oe
 
 cd "$HOME/projects/training-lucy" || exit 1
